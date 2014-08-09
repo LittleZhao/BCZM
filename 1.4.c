8aa8aa643bfdb561310109d8bf21374c523d0092
Zhao <LittleZhao@users.noreply.github.com>
2014-08-09 19:15:30 +0800

    initialize

---
+++ 1.4.c
@@ -0,0 +1,37 @@
+//cup is dual core, only get to 50% cpu usage.
+#include <Windows.h>
+#include <stdlib.h>
+#include <math.h>
+
+const double SPLIT = 0.01;//split*count should be 2
+const int COUNT = 200;
+const int INTERVAL = 600;
+
+int main(void)
+{
+    unsigned long busySpan[COUNT];
+    unsigned long idleSpan[COUNT];
+    unsigned long startTime;
+    int half = INTERVAL/2;
+    double radian = 0.0;
+    int i;
+
+    for (i=0; i<COUNT; i++)
+    {
+        busySpan[i] = (unsigned long)(half + (sin(radian*M_PI) * half));
+        idleSpan[i] = INTERVAL - busySpan[i];
+        radian += SPLIT;
+    }
+
+    i=0;
+    while (1)
+    {
+        i %= COUNT;
+        startTime = GetTickCount();
+        while (GetTickCount() - startTime <= busySpan[i]);
+
+        Sleep(idleSpan[i]);
+        i++;
+    }
+    return 0;
+}
